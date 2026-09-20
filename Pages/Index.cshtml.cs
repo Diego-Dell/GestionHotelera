0921bf8960f3c03da5a9dcc7f87b4786dd9df5bb
Diego-Dell <diegoaprende06@gmail.com>
2026-09-20 10:41:35 -0400

    Agregar archivos de proyecto.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GestionHotelera.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
